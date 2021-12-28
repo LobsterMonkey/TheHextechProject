849695d822a6ee1cc57cbc4c08c1050627728783
LobsterMonkey <hugo.fontaine.hsu@gmail.com>
2021-12-28 11:05:29 -0800

    Change burn mechanism & unit tests for hextech token

--- contracts/HexTechToken.sol
+++ contracts/HexTechToken.sol
@@ -5,6 +5,7 @@ pragma solidity 0.8.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "hardhat/console.sol";
 
 contract HexTechToken is ERC20, AccessControlEnumerable {
 
@@ -16,28 +17,40 @@ contract HexTechToken is ERC20, AccessControlEnumerable {
          _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
 
-    function mint(address to, uint amount) external onlyRole(MINTER_ROLE) {
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    function burn(address account, uint amount) external onlyRole(BURNER_ROLE) {
-        _burn(account, amount);
+    function burn(address from, uint256 amount) external onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override {
-        
-        if (from != address(0) && to != address(0) && !hasRole(MINTER_ROLE, _msgSender())) {
-            //transfer
-            uint256 tokensToBurn = amount.div(100);
-            amount = amount.sub(tokensToBurn);
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        return super.transfer(to, _partialBurn(amount));
+    }
 
-            _burn(_msgSender(), tokensToBurn);
+    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
+        return super.transferFrom(from, to, _partialBurn(amount));
+    }
+
+    function _partialBurn(uint256 amount) internal returns (uint256) {
+        uint256 burnAmount = _calculateBurnAmount(amount);
+
+        if (burnAmount > 0) {
+            _burn(msg.sender, burnAmount);
         }
 
-        super._beforeTokenTransfer(from, to, amount);
+        return amount.sub(burnAmount);
     }
+
+    function _calculateBurnAmount(uint256 amount) internal view returns (uint256) {
+        uint256 burnAmount = 0;
+
+        if (!hasRole(MINTER_ROLE, msg.sender)) {
+            burnAmount = amount.div(100);
+        }
+
+        return burnAmount;
+    }
+        
 }
---
+++ test/HextechToken-test.js
@@ -0,0 +1,79 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Hextech Token Contract", function () {
+
+    let HexTechToken;
+    let instanceHexTechToken;
+
+    beforeEach(async function () {
+
+        [owner, addr1, addr2, addr3, addr4, addr5] = await ethers.getSigners();
+
+        HexTechToken = await ethers.getContractFactory("HexTechToken");
+        instanceHexTechToken = await HexTechToken.deploy("HXT", "Hextech Token");
+    });
+
+    it("should set the right default admin", async function () {
+        expect(await instanceHexTechToken.hasRole("0x0000000000000000000000000000000000000000000000000000000000000000", owner.address)).to.be.true;
+    });
+
+    it("should grant minter role", async function () {
+        await instanceHexTechToken.grantRole(ethers.utils.id("MINTER_ROLE"),owner.address);
+        expect(await instanceHexTechToken.hasRole(ethers.utils.id("MINTER_ROLE"), owner.address)).to.be.true;
+    });
+
+    it("should grant burner role", async function () {
+        await instanceHexTechToken.grantRole(ethers.utils.id("BURNER_ROLE"),owner.address);
+        expect(await instanceHexTechToken.hasRole(ethers.utils.id("BURNER_ROLE"), owner.address)).to.be.true;
+    });
+
+    it("should allow minter to mint", async function () {
+        await instanceHexTechToken.grantRole(ethers.utils.id("MINTER_ROLE"),addr1.address);
+        await instanceHexTechToken.connect(addr1).mint(addr1.address, ethers.utils.parseUnits("1000","ether"));
+
+        expect(await instanceHexTechToken.balanceOf(addr1.address)).to.be.equal(ethers.utils.parseUnits("1000","ether"));
+    });
+
+    it("should not allow non minter to mint", async function () {
+        await expect(instanceHexTechToken.connect(addr1).mint(addr1.address, ethers.utils.parseUnits("1000","ether"))).to.be.revertedWith('AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6');
+    });
+
+    it("should allow burner to burn", async function () {
+        await instanceHexTechToken.grantRole(ethers.utils.id("MINTER_ROLE"),addr1.address);
+        await instanceHexTechToken.connect(addr1).mint(addr1.address, ethers.utils.parseUnits("1000","ether"));
+
+        await instanceHexTechToken.grantRole(ethers.utils.id("BURNER_ROLE"),addr1.address);
+        await instanceHexTechToken.connect(addr1).burn(addr1.address, ethers.utils.parseUnits("500","ether"));
+
+        expect(await instanceHexTechToken.balanceOf(addr1.address)).to.be.equal(ethers.utils.parseUnits("500","ether"));
+    });
+
+    it("should not allow non burner to burn", async function () {
+        await instanceHexTechToken.grantRole(ethers.utils.id("MINTER_ROLE"),addr1.address);
+        await instanceHexTechToken.connect(addr1).mint(addr1.address, ethers.utils.parseUnits("1000","ether"));
+
+        await expect(instanceHexTechToken.connect(addr1).burn(addr1.address, ethers.utils.parseUnits("500","ether"))).to.be.revertedWith('AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role 0x3c11d16cbaffd01df69ce1c404f6340ee057498f5f00246190ea54220576a848');
+    });
+
+    it("should burn 1% of transfer", async function () {
+        await instanceHexTechToken.grantRole(ethers.utils.id("MINTER_ROLE"),owner.address);
+        await instanceHexTechToken.mint(addr2.address, ethers.utils.parseUnits("1000","ether"));
+        expect(await instanceHexTechToken.balanceOf(addr2.address)).to.be.equal(ethers.utils.parseUnits("1000","ether"));
+
+        await instanceHexTechToken.connect(addr2).transfer(owner.address, ethers.utils.parseUnits("500","ether"));
+
+        expect(await instanceHexTechToken.balanceOf(owner.address)).to.be.equal(ethers.utils.parseUnits("495","ether"));
+    });
+
+    it("it should not burn 1% of transfer if made by minter", async function () {
+        await instanceHexTechToken.grantRole(ethers.utils.id("MINTER_ROLE"),owner.address);
+        await instanceHexTechToken.mint(owner.address, ethers.utils.parseUnits("1000","ether"));
+        expect(await instanceHexTechToken.balanceOf(owner.address)).to.be.equal(ethers.utils.parseUnits("1000","ether"));
+
+        await instanceHexTechToken.transfer(addr2.address, ethers.utils.parseUnits("500","ether"));
+
+        expect(await instanceHexTechToken.balanceOf(addr2.address)).to.be.equal(ethers.utils.parseUnits("500","ether"));
+    });
+    
+});
